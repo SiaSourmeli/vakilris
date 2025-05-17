9b7be128998ca00091e23c28737cbe27728aa96f
SiaSourmeli <athanasia.sourmeli@gmail.com>
2025-05-17 21:01:16 +0300

    desing news page, added contentful in pages and news

--- pages/people.js
+++ pages/people.js
@@ -1,22 +1,33 @@
 import Layout from "../components/layout";
 import Card from "../components/card";
 import styles from "../styles/people.module.css";
-import people from "../content/people.json";
+import { getEntries } from "../library/contentful";
 
-export default function People() {
+
+export async function getStaticProps() {
+ const people = await getEntries("people", {
+    order: 'fields.orderNo',
+  });
+  return {
+    props: {
+      people,
+    },
+  };
+}
+
+
+export default function People({people}) {
   return (
     <Layout>
       <div id="people">
         <h1 className="header">People</h1>
         <div className={styles.peopleContainer}>
-          {people.map((person, index) => (
-            <Card key={index} {...person} />
+          {people.map((person) => (
+            <Card key={person.sys.id} {...person.fields} />
           ))}
         </div>
       </div>
-
       <hr className={styles.hr}></hr>
-
       <section className={styles.joinUsContainer}>
         <Card
           photo={
